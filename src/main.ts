1124e007657fdf2302336ff64ce52602799022b5
jmedina <jmedinalba@gmail.com>
2025-10-16 11:49:23 +0200

    commit correcciones

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 let puntuacionUsuario: number = 0;
 const mensaje = document.getElementById("mensaje-juego");
+let cartaVisible = document.getElementById("carta-visible");
 
 const muestraPuntuacion = (): void => {
   let puntuacion = document.getElementById("puntuacion");
@@ -10,12 +11,15 @@ const muestraPuntuacion = (): void => {
 
 document.addEventListener("DOMContentLoaded", muestraPuntuacion);
 
-const dameCarta = (): number => {
-  let cartaAleatoria: number = Math.floor(Math.random() * 10) + 1;
-  if (cartaAleatoria > 7) {
-    cartaAleatoria = cartaAleatoria + 2;
+const dameNumeroAleatorio = () => {
+  return Math.floor(Math.random() * 10) + 1;
+};
+
+const dameCarta = (numeroAleatorio: number): number => {
+  if (numeroAleatorio > 7) {
+    numeroAleatorio = numeroAleatorio + 2;
   }
-  return cartaAleatoria;
+  return numeroAleatorio;
 };
 
 const obtenerValorCarta = (carta: number): number => {
@@ -25,11 +29,8 @@ const obtenerValorCarta = (carta: number): number => {
   return carta;
 };
 
-const mostrarCarta = (cartaAleatoria: number): void => {
-  let cartaVisible = document.getElementById("carta-visible");
+const obtenerUrlCarta = (cartaAleatoria: number): string => {
   let rutaImagen = "";
-  let mensajeDefault = "";
-
   switch (cartaAleatoria) {
     case 1:
       rutaImagen =
@@ -72,16 +73,42 @@ const mostrarCarta = (cartaAleatoria: number): void => {
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/cartas/copas/12_rey-copas.jpg";
       break;
     default:
-      mensajeDefault = "Ups! Parece que algo ha fallado. Vuelve a empezar";
+      return "Ups! Parece que algo ha fallado. Vuelve a empezar";
       break;
   }
+  return rutaImagen;
+};
 
+const mostrarUrlCarta = (urlCarta: string) => {
   if (cartaVisible instanceof HTMLImageElement) {
-    cartaVisible.src = rutaImagen;
+    cartaVisible.src = urlCarta;
   } else {
     if (cartaVisible !== null && cartaVisible !== undefined) {
-      cartaVisible.innerHTML = mensajeDefault;
+      cartaVisible.innerHTML =
+        "Ups! Parece que algo ha fallado. Vuelve a empezar";
     }
+  }
+};
+
+const sumarPuntos = (puntosCarta: number) => {
+  return puntuacionUsuario + puntosCarta;
+};
+
+const actualizarPuntuacionUsuario = (nuevosPuntos: number) => {
+  puntuacionUsuario = nuevosPuntos;
+};
+
+const dameCartaF = () => {
+  const numeroAleatorio = dameNumeroAleatorio();
+  const cartaObtenida = dameCarta(numeroAleatorio);
+  const url = obtenerUrlCarta(cartaObtenida);
+  mostrarUrlCarta(url);
+  const valorPuntos = obtenerValorCarta(cartaObtenida);
+  const puntosSumados = sumarPuntos(valorPuntos);
+  actualizarPuntuacionUsuario(puntosSumados);
+  muestraPuntuacion();
+  if (puntuacionUsuario > 7.5) {
+    return finalizarPartida();
   }
 };
 
@@ -92,31 +119,42 @@ if (
   btnDameCarta instanceof HTMLButtonElement
 ) {
   btnDameCarta.addEventListener("click", () => {
-    const cartaObtenida = dameCarta();
-    mostrarCarta(cartaObtenida);
-    const valorPuntos = obtenerValorCarta(cartaObtenida);
-    puntuacionUsuario += valorPuntos;
-    muestraPuntuacion();
-    if (puntuacionUsuario > 7.5) {
-      btnDameCarta.disabled = true;
-      if (mensaje) {
-        mensaje.innerHTML = "Game Over";
-      }
-      return finalizarPartida();
-    }
+    dameCartaF();
   });
 }
 
 const btnMePlanto = document.getElementById("btnMePlanto");
 const btnNuevaPartida = document.getElementById("btnNuevaPartida");
 
+const resetearPartida = (): void => {
+  puntuacionUsuario = 0;
+  muestraPuntuacion();
+  if (cartaVisible instanceof HTMLImageElement) {
+    cartaVisible.src =
+      "https://github.com/Lemoncode/fotos-ejemplos/blob/main/cartas/back.jpg?raw=true";
+  }
+  if (mensaje) {
+    mensaje.innerHTML = "";
+  }
+  if (btnDameCarta instanceof HTMLButtonElement) {
+    btnDameCarta.disabled = false;
+  }
+  if (btnMePlanto instanceof HTMLButtonElement) {
+    btnMePlanto.disabled = false;
+  }
+  if (btnNuevaPartida) {
+    btnNuevaPartida.classList.add("oculto");
+  }
+};
+
 if (btnNuevaPartida) {
-  btnNuevaPartida.addEventListener("click", () => {
-    window.location.reload();
-  });
+  btnNuevaPartida.addEventListener("click", resetearPartida);
 }
 
 const finalizarPartida = (): void => {
+  if (mensaje) {
+    mensaje.innerHTML = obtenerMensaje();
+  }
   if (btnNuevaPartida) {
     btnNuevaPartida.classList.remove("oculto");
   }
@@ -128,6 +166,28 @@ const finalizarPartida = (): void => {
   }
 };
 
+const obtenerMensaje = (): string => {
+  if (puntuacionUsuario === 7.5) {
+    return "¡Lo has clavado!¡Enhorabuena!";
+  } else if (puntuacionUsuario > 7.5) {
+    return "Game Over";
+  } else if (puntuacionUsuario < 5) {
+    return "Has sido muy conservador";
+  } else if (puntuacionUsuario === 5) {
+    return "Te ha entrado el canguelo, eh?";
+  } else if (puntuacionUsuario >= 5) {
+    return "Casi casi...";
+  } else {
+    return "Upss... No tendrías que pasar por aquí";
+  }
+};
+
+const mostrarMensaje = (mensajeCuandoMePlanto: string) => {
+  if (mensaje instanceof HTMLDivElement) {
+    mensaje.innerHTML = mensajeCuandoMePlanto;
+  }
+};
+
 if (
   btnMePlanto &&
   btnMePlanto instanceof HTMLButtonElement &&
@@ -136,15 +196,9 @@ if (
   btnMePlanto.addEventListener("click", () => {
     if (puntuacionUsuario > 7.5) {
       btnMePlanto.disabled = true;
-    } else if (puntuacionUsuario === 7.5) {
-      mensaje.innerHTML = "¡Lo has clavado!¡Enhorabuena!";
-    } else if (puntuacionUsuario < 5) {
-      mensaje.innerHTML = "Has sido muy conservador";
-    } else if (puntuacionUsuario === 5) {
-      mensaje.innerHTML = "Te ha entrado el canguelo, eh?";
-    } else if (puntuacionUsuario >= 5) {
-      mensaje.innerHTML = "Casi casi...";
     }
+    const mensaje = obtenerMensaje();
+    mostrarMensaje(mensaje);
     finalizarPartida();
   });
 }
